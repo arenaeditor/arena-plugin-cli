d6fdb0f7c9bcc28a5c31b3cc6ca84ac0206120e6
何文力 <hewenli@360.cn>
2019-07-16 15:15:59 +0800

    feat(compiler): plugin loader

--- src/ipc/index.js
+++ src/ipc/index.js
@@ -1,7 +1,8 @@
 const ipc = require('node-ipc')
 const fs = require('fs')
-const unixSocketPath = '/tmp/cn.360.arena.plugin'
+// const unixSocketPath = '/tmp/cn.360.arena.plugin'
 const connectionId = 'ArenaPluginSocketService'
+const universalTCPPort = 9999;
 ipc.config.encoding = 'utf8'
 ipc.config.silent = true
 ipc.config.id = Math.random().toString(36).slice(2)
@@ -13,11 +14,11 @@ class ArenaPluginIPCClient {
   }
 
   async connect() {
-    if (!fs.existsSync(unixSocketPath)) {
-      return '插件宿主不存在，请先启动 Arena'
-    }
+    // if (!fs.existsSync(unixSocketPath)) {
+    //   return '插件宿主不存在，请先启动 Arena'
+    // }
     return new Promise(resolve => {
-      ipc.connectTo(connectionId, unixSocketPath, () => {
+      ipc.connectToNet(connectionId, undefined, universalTCPPort, () => {
         resolve()
       })
     })
