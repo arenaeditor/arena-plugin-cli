97e8364d64e9baef29931cb5847af04c69d2603c
何文力 <hewenli@360.cn>
2019-11-06 17:04:25 +0800

    feat: publish flag

--- src/commands/build.js
+++ src/commands/build.js
@@ -1,11 +1,13 @@
 const {Command, flags} = require('@oclif/command')
-const writer = require('arena-file/writer');
+const writer = require('arena-file/writer')
 const webpackCompiler = require('../webpack/compiler')
 const t = require('../ui')
 const zlib = require('zlib')
 const fs = require('fs')
 const path = require('path')
+const request = require('request')
 const semver = require('semver')
+const userToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkb2MiOiI1ZDY0OWY0YmRlZGM0ZTMyY2Y1NDU4NjYiLCJ1biI6Imhld2VubGkiLCJlbWFpbCI6Imhld2VubGlAMzYwLmNuIiwibmlja25hbWUiOiLkvZXmloflipsiLCJwZXJtcyI6WyJVU0VSIl0sImlzQWRtaW4iOmZhbHNlLCJhZG1pbiI6e30sImlhdCI6MTU3MjMyMDg5MiwiZXhwIjoxNTcyOTI1NjkyLCJpc3MiOiJBUkVOQV9URVNUIn0.0pBfVkcEm__DRU_Qc7PxwlifksgnjFJJlgBi0t_NbIM'
 
 class BuildCommand extends Command {
   async run() {
@@ -52,7 +54,7 @@ class BuildCommand extends Command {
     }
 
     if (content.config.version) {
-      content.config.version = semver.inc(content.config.version, flags.bump)
+      // content.config.version = semver.inc(content.config.version, flags.bump)
       const pluginJson = path.resolve(process.cwd(), 'plugin.json')
       const jsonOnDisk = fs.readFileSync(pluginJson, {encoding: 'utf8'})
       const jsonContent = JSON.parse(jsonOnDisk)
@@ -67,23 +69,48 @@ class BuildCommand extends Command {
 
     const w = new writer(savePath)
     w.setContentVersion(content.config.version || '0.0.0')
-    w.addContent(contentBuffer, 'content.json')
-    w.addContent(fileBuffer.code, 'code.js')
+    w.addContent(contentBuffer, 'content.json', true)
+    w.addContent(fileBuffer.code, 'code.js', true)
     for (let index = 0; index < content.config.plugins.length; index += 1) {
       const p = content.config.plugins[index]
-      p.icon && w.addFile(path.resolve(process.cwd(), p.icon), p.icon)
-      p.thumb && w.addFile(path.resolve(process.cwd(), p.thumb), p.thumb)
+      p.icon && w.addFile(path.resolve(process.cwd(), p.icon), p.icon, false)
+      p.thumb && w.addFile(path.resolve(process.cwd(), p.thumb), p.thumb, false)
     }
-    content.config.thumb && w.addFile(path.resolve(process.cwd(), content.config.thumb), content.config.thumb)
+    content.config.thumb && w.addFile(path.resolve(process.cwd(), content.config.thumb), content.config.thumb, false)
 
     // add style
     fileBuffer.styles.forEach(style => {
       w.addContent(style.style, style.name)
     })
 
-    w.write()
+    w.write().then(() => {
+      if (flags.publish) {
+        t.term.defaultColor('正在发布插件')
+        this.uploadPlugin(savePath, content.config)
+      }
+    })
 
     // fs.writeFileSync(savePath, compressedBuffer)
+  }
+
+  /**
+   * 上传本地打包好的文件
+   * @param filePath
+   * @param config
+   */
+  uploadPlugin(filePath, config) {
+    request({
+      headers: {
+        'Access-Token': userToken,
+      },
+      formData: {
+        file: fs.createReadStream(filePath),
+        id: config.pluginId,
+        version: config.version,
+      },
+      method: 'POST',
+      url: 'http://arena.qiwoo.org/api/license/publish',
+    })
   }
 
   compileWarning(content) {
@@ -110,6 +137,11 @@ BuildCommand.flags = {
   delete: flags.boolean({
     char: 'd',
     description: 'Delete old version',
+  }),
+  publish: flags.boolean({
+    char: 'p',
+    description: 'Publish to Arena Market',
+    default: false,
   }),
 }
 
